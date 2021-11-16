79fe8fcb4455dbb1c3d4820299977e392a9ad69d
nandotezza <fernando.tezza@gmail.com>
2021-11-15 21:28:44 -0300

    Update basic logic for the counter

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,14 @@ const numberHours = document.querySelector('.number-hours');
 const numberMinutes = document.querySelector('.number-minutes');
 const numberSeconds = document.querySelector('.number-seconds');
 
-console.log( `counterBtn = ${counterBtn} - modal = ${modal} - modalBtn = ${modalBtn}`);
+const selectTheDate = document.querySelector('.thedate');
+const selectTheTime = document.querySelector('.thetime');
+
+
+
+let counterStatus = 0;
+
+// console.log( `counterBtn = ${counterBtn} - modal = ${modal} - modalBtn = ${modalBtn}`);
 
 // Open and Close modal by clicking on counterBtn
 counterBtn.addEventListener('click', toggleModal);
@@ -20,41 +27,85 @@ function toggleModal() {
 
 
 
-modalBtn.addEventListener('click', () => {
-    timerID = setInterval(runCountdown, 1000);
-});
+modalBtn.addEventListener('click', verifyStatus);
 
+function verifyStatus() {
+    // Verifies if counter is running
+    if(counterStatus === 2) {
+        clearInterval(timerID);
+        counterStatus = 0;
+        console.log('Counter stopped');
+        theDate._input.removeAttribute('disabled');
+        theTime._input.removeAttribute('disabled');
+        return;
+    }
+
+    if (counterStatus === 0) {
+        // console.log(`Status = ${counterStatus}`);
+
+        dateField = document.querySelector('.thedate.flatpickr-input');
+        timeField = document.querySelector('.thetime.flatpickr-input');
+
+        // dateOutput = dateField.value;
+        // timeOutput = timeField.value;
+
+    }    if( (dateField.value && timeField.value) != undefined && (dateField.value && timeField.value) != null && (dateField.value && timeField.value) !== '')  {
+
+            counterStatus === 2 ? counterStatus = 2 : counterStatus = 1;
+
+            dateOutput = dateField.value;
+            timeOutput = timeField.value;
+
+            // console.log(`Date = ${dateOutput} and Time = ${timeOutput} Status = ${counterStatus}`);
+
+            timerID = setInterval(runCountdown, 1000);
+        } 
+}
 
 function runCountdown() {
     if(timerID)
-        getDateTime();
-    else
+        calcInterval();
+    else {
         console.log('error');
         return;
-
+    }
 }
 
-function getDateTime() {    
-    const dateOutput = document.querySelector('.thedate.flatpickr-input').value;
-    const timeOutput = document.querySelector('.thetime.flatpickr-input').value;
-    console.log(`Date = ${dateOutput} and Time = ${timeOutput}`);
-    calcInterval(dateOutput, timeOutput);
-}
-
-
-function calcInterval(dateOutput, timeOutput) {
+function calcInterval() {
     const launchDate = new Date(`${dateOutput}, ${timeOutput}`).getTime();
     const now = new Date().getTime();
 
-    if (launchDate <= now) {
-        clearInterval(timerID);
-        alert('Time has expired');
-        return
-    }    
+    // console.log(launchDate);
 
-    const difference = launchDate - now;
-    console.log(`difference = ${difference}`);
+    if (launchDate < now || isNaN(launchDate) || launchDate === undefined || launchDate === null || launchDate === '') {
+
+        switch (launchDate) {
+            case isNaN:
+                break;
+        }
+        console.log('You must select a future time');
+        clearInterval(timerID);
+        return;
+       
+        
+        
+    }  
+
+
+        // if(counterStatus !== 0) {
+        //     clearInterval(timerID);
+        //     console.log('Time has expired');
+        //     return;
+        // }              
+    
+    const difference = launchDate - now;    
     calcTime(difference);
+
+    // console.log (`launchdate = ${launchDate}`);
+    // console.log(`difference = ${difference}`);
+    
+
+    
 }
 
 function calcTime(difference) {
@@ -68,10 +119,19 @@ function calcTime(difference) {
     let timeMinutes = Math.floor((difference % hours) / minutes);
     let timeSeconds = Math.floor((difference % minutes) / seconds);
 
-    let displaytime = timeDays < 10 ? ` |0${timeDays} days ` : ` |${timeDays} days `;
-    displaytime += timeHours < 10 ? ` |0${timeHours} hours ` : ` |${timeHours} hours`;
-    displaytime += timeMinutes < 10 ? ` |0${timeMinutes} minutes ` : ` |${timeMinutes} minutes `;
-    displaytime += timeSeconds < 10 ? ` |0${timeSeconds} seconds |` : ` |${timeSeconds} seconds |`;
+    timeDays = timeDays < 10? `0${timeDays}` : `${timeDays}`;
+    timeHours = timeHours < 10 ? `0${timeHours}` : `${timeHours}`;
+    timeMinutes = timeMinutes < 10 ? `0${timeMinutes}` : `${timeMinutes}`;
+    timeSeconds = timeSeconds < 10 ? `0${timeSeconds}` : `${timeSeconds}`;
+
+    counterStatus = 2;
+    theDate._input.setAttribute('disabled', 'disabled');
+    theTime._input.setAttribute('disabled', 'disabled');
+    // selectTheDate.disabled = true;
+    // selectTheTime.disabled = true;
+
+    let displaytime = `| ${timeDays} days |${timeHours} hours |${timeMinutes} minutes ${timeSeconds} seconds |`; 
+   
 
     numberDays.innerHTML = timeDays;
     numberHours.innerHTML = timeHours;
@@ -83,10 +143,13 @@ function calcTime(difference) {
 
 
 
+
+
 const theDate = flatpickr(".thedate", {
     altInput: true,
     altFormat: "F j, Y",
     dateFormat: "Y-m-d",
+    minDate: new Date()
 });
 
 const theTime = flatpickr(".thetime", {
@@ -94,11 +157,14 @@ const theTime = flatpickr(".thetime", {
     noCalendar: true,
     dateFormat: "H:i",
     time_24hr: true,
-    defaultHour:new Date().getHours(),
-    defaultMinute:new Date().getMinutes()
+    // minTime: new Date(),
+    // minTime: new Date().getHours(),
+    minuteIncrement: 1,
+    defaultHour: new Date().getHours(),
+    defaultMinute: new Date().getMinutes()
 });
 
 
-console.log(theDate.selectedDates);
-console.log(theTime.selectedDates);
+// console.log(theDate.selectedDates);
+// console.log(theTime.selectedDates);
 
